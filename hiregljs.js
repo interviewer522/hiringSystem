1b5d4689cd9411548c7e7ac26962b109de632813
brandoburj <jan.brandobur@gmail.com>
2017-04-03 18:44:55 +0200

    basic layout in progress

---
+++ hiregljs.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#my-interviews").click(function(){
+		$("#sub-heading").text("My Interviews");
+	});
+	$("#new-interview").click(function(){
+		$("#sub-heading").text("New Interview");
+	});
+});
