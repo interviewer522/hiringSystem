089174cd86cb89cd68b9570e74919d5b03378c92
brandoburj <jan.brandobur@gmail.com>
2017-04-09 15:30:19 +0200

    input,select, login design improvements

---
+++ js/hiregljs.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("#my-interviews").click(function(){
+	$("#sub-heading").text("My Interviews");
+  });
+  $("#new-interview").click(function(){
+	$("#sub-heading").text("New Interview");	
+  });
+  $("#side-bottom-bar > a").click(function() {    
+    var x = $(this).index();   
+    $("#table-interviews").toggle(x===0);
+    $("#content-footer, #tab-interview, #tab-candidate").toggle(x===1);    
+  });
+  $("#closeIcon").click(function(){
+    $(".modal").hide();
+  });
+});
